3b53506b05f565b84fe5a56b5c609e21e5d99b37
Mateo <78664744+mateofinanceone@users.noreply.github.com>
2021-03-10 22:27:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-console.log('Starting...')
+console.log('Mulai...')
 let { spawn } = require('child_process')
 let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts  = require('cfonts')
-CFonts.say('Lightweight\nWhatsApp Bot', {
+CFonts.say('RUBY2007TC\nXRUBYYY', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
