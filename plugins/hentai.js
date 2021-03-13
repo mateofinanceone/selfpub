fc3510d2b88bce2ad980f06cc3c3fcc119d1ab14
Mateo <78664744+mateofinanceone@users.noreply.github.com>
2021-03-13 06:16:47 +0000

    Update hentai.js

--- plugins/hentai.js
+++ plugins/hentai.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn }) => {
- await conn.sendFile(m.chat, global.API('xteam', '/randomimage/hentai', {}, 'APIKEY'), 'nsfw.png', 'Fix buat bacol', m)
+ await conn.sendFile(m.chat, global.API('xteam', '/randomimage/hentai', {}, 'APIKEY'), 'nsfw.png', 'Baka >///<', m)
 }
 handler.help = ['hentai']
 handler.tags = ['nsfw']
