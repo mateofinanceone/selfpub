9e0703562bacd8c9fd21eee40c62ce4e6969da5c
Mateo <78664744+mateofinanceone@users.noreply.github.com>
2021-03-13 06:19:01 +0000

    Update mstrb.js

--- plugins/mstrb.js
+++ plugins/mstrb.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn }) => {
- await conn.sendFile(m.chat, global.API('xteam', '/randomimage/mstrb', {}, 'APIKEY'), 'nsfw.png', 'Fix buat bacol', m)
+ await conn.sendFile(m.chat, global.API('xteam', '/randomimage/mstrb', {}, 'APIKEY'), 'nsfw.png', 'Baka >///<', m)
 }
 handler.help = ['mstrb']
 handler.tags = ['nsfw']
