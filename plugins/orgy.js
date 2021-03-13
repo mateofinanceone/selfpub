905d323704b0f840e92780201ecde5cb33af50c5
Mateo <78664744+mateofinanceone@users.noreply.github.com>
2021-03-13 06:21:29 +0000

    Update orgy.js

--- plugins/orgy.js
+++ plugins/orgy.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn }) => {
- await conn.sendFile(m.chat, global.API('xteam', '/randomimage/orgy', {}, 'APIKEY'), 'nsfw.png', 'Fix buat bacol', m)
+ await conn.sendFile(m.chat, global.API('xteam', '/randomimage/orgy', {}, 'APIKEY'), 'nsfw.png', 'Baka >///<', m)
 }
 handler.help = ['orgy']
 handler.tags = ['nsfw']
